4944fc338937ab16b41e43eb11640791f57b50d0
amagarzogonzalo <amagarzogonzalo@gmail.com>
2024-02-12 16:38:54 +0100

    image loop

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -1,6 +1,4 @@
 import cv2
-import os
-from os import listdir
 
 def click_event(event, x, y, flags, params): 
     # checking for left mouse clicks 
@@ -8,13 +6,26 @@ def click_event(event, x, y, flags, params):
         # displaying the coordinates 
         # on the Shell 
         print(x, ' ', y) 
-        return x, y
        
-print("h")
-# get the path/directory
-folder_dir = "C:/Users/Alex/Desktop/AI/Workspace/Computer Vision/computer_vision/Assignment 1/images"
-for image in os.listdir(folder_dir):
-    img = cv2.imread(image, 1) 
+def corner_detection (img):
+
+    # Find the chess board corners
+
+    # if chessboard corners are detected
+    if ret == True:
+    
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (7,6), corners,ret)
+        cv2.imshow('Chessboard',img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+  
+
+
+if __name__=="__main__": 
+  
+    # reading the image 
+    img = cv2.imread('lena.jpg', 1) 
     # convert the input image to a grayscale
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # displaying the image 
@@ -27,10 +38,10 @@ for image in os.listdir(folder_dir):
     
         # wait for a key to be pressed to exit 
         cv2.waitKey(0) 
+    
         # close the window 
         cv2.destroyAllWindows() 
-    else:
-        a = 2  
-        print("ok")      
-print("finish")
 
+    else :
+    
+  
