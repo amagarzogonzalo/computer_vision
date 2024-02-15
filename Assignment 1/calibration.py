45eb05f3a5eb9b148314f861dce9eb1e5a4710b9
amagarzogonzalo <amagarzogonzalo@gmail.com>
2024-02-15 18:09:47 +0100

    obtainining error from calibration

--- Assignment 1/calibration.py
+++ Assignment 1/calibration.py
@@ -1,4 +1,57 @@
 import cv2
 
 def calibration(objpoints, imgpoints, gray):
+    """
+    The function that calculates the calibration.
+    :param objpoints: Object points in the real world.
+    :param imgpoints: Image points in the image plane.
+    :param gray: The grayscale image used for calibration.
+    :return: ret: Boolean indicating successful calibration.
+             mtx: Camera matrix.
+             dist: Distortion coefficients.
+             rvecs: Rotation vectors.
+             tvecs: Translation vector
+    
+    """
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    return ret, mtx, dist, rvecs, tvecs
+
+def undistort (img, mtx, dist, image_i):
+    """
+    Perform undistortion on an image.
+    
+    :param img: The input image to undistort.
+    :param mtx: The camera matrix.
+    :param dist: The distortion coefficients.
+    :param image_i: Name of the image in the folder. 
+    :return: The undistorted image.
+    """
+    h,  w = img.shape[:2]
+    # The new camera matrix for undistortion.
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    # undistort
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv2.imwrite(image_i, dst)
+
+
+def compute_error(objpoints, imgpoints, rvecs, tvecs, mtx, dist):
+    """
+    Compute the reprojection error for camera calibration.
+    
+    :param objpoints: The object points in the real world.
+    :param imgpoints: The image points in the image plane.
+    :param rvecs: The rotation vectors.
+    :param tvecs: The translation vectors.
+    :param mtx: The camera matrix.
+    :param dist: The distortion coefficients.
+    """ 
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+        mean_error += error
+    total_error = mean_error/len(objpoints)
+    print(f"Total error: {total_error}. Mean Error: {mean_error}. Number of Object Points (Real World): {len(objpoints)} ")
--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from interpolate import interpolate, reverse_again
-from calibration import calibration
+from calibration import calibration, undistort, compute_error
 import os
 from os import listdir
 
@@ -13,7 +13,7 @@ def load_and_resize_image(image_path, scale_x=0.9, scale_y=0.9):
 
 def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, ' ', y)
+        #print(x, ' ', y)
         corner_points.append((x, y))
         cv2.circle(img, (x, y), 3, (255, 0, 0), -1)
         cv2.imshow('Image', img)
@@ -29,7 +29,7 @@ def see_window(window_name, image):
 def find_and_draw_chessboard_corners(image, chessboard_size, criteria):
     ret, corners = cv2.findChessboardCorners(image, chessboard_size, None)
     if ret:
-        print("Chessboard corners found:", corners)
+        print("Chessboard corners found.")
         corners2 =  corners #cv2.cornerSubPix(image, corners, (11,11), (-1,-1), criteria)
         cv2.drawChessboardCorners(img, chessboard_size, corners2, ret)
         see_window("Detected corners automatically", image)
@@ -42,12 +42,15 @@ def find_and_draw_chessboard_corners(image, chessboard_size, criteria):
         cv2.setMouseCallback('Image', click_event, corners)
         cv2.waitKey(0)
         image_interpolated, corners_interpolated = interpolate(image, corners, chessboard_size)
-        corners2 = corners # cv2.cornerSubPix(image_interpolated, corners_interpolated, (11,11), (-1,-1), criteria)
-        cv2.drawChessboardCorners(image_interpolated, chessboard_size, corners2, True)
-        final_image = reverse_again (image,image_interpolated, corners_interpolated, corners)
+        if image_interpolated is None or corners_interpolated is None:
+            return None
+        else: 
+            corners2 = corners_interpolated # cv2.cornerSubPix(image_interpolated, corners_interpolated, (11,11), (-1,-1), criteria)
+            cv2.drawChessboardCorners(image_interpolated, chessboard_size, corners2, True)
+            final_image = reverse_again (image,image_interpolated, corners_interpolated, corners)
 
-        see_window("Result with Interpolation", final_image)
-        return corners2
+            see_window("Result with Interpolation", final_image)
+            return corners2
 
 def draw_chessboard_corners(img, corners, chessboard_size):
     ret = 4
@@ -58,10 +61,14 @@ def draw_chessboard_corners(img, corners, chessboard_size):
 if __name__ == "__main__":
     corner_points = []
     chessboard_size = (6, 9)
-    obj_points = []
-    img_points = []
-    objp = np.zeros((6*7,3), np.float32)
-    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    square_size = 22
+    objpoints = []
+    imgpoints = []
+    objp = np.zeros((6*9,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+    objp[:,:2]=objp[:,:2]*square_size 
+
+    
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)    
     
@@ -73,15 +80,21 @@ if __name__ == "__main__":
         image_path = os.path.join(folder_path, image_i)
         print(f"Attempting image: {image_i}")
         img_aux = load_and_resize_image(image_path)
+        # img is gray image
         img = cv2.cvtColor(img_aux, cv2.COLOR_BGR2GRAY)
 
         corners2 = find_and_draw_chessboard_corners(img, chessboard_size, criteria) 
         if corners2 is not None and len(corners2) > 0: 
-            img_points.append(corners2)
-            obj_points.append(objp)
+            imgpoints.append(corners2)
+            objpoints.append(objp)
             cv2.waitKey(0)  
+            
+            
         else:
             print(f"No corners found for image {image_i}")
 
+    print("Compute Error: ")
+    ret, mtx, dist, rvecs, tvecs = calibration(objpoints, imgpoints,  img)
+    compute_error(objpoints, imgpoints, rvecs, tvecs, mtx, dist)
 
     cv2.destroyAllWindows()
