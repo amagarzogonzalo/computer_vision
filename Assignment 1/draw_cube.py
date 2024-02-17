cc4e6ce0dcc50e95b4dbbecc267ed7105d474b24
mamlu007 <76781346+mamlu007@users.noreply.github.com>
2024-02-17 20:27:44 +0100

    t

--- Assignment 1/draw_cube.py
+++ Assignment 1/draw_cube.py
@@ -2,14 +2,27 @@ import cv2
 import numpy as np
 
 def draw_cube(img, corners, imgpts):
-    imgpts = np.int32(imgpts).reshape(-1,2)
-    img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),3)
-    for i,j in zip(range(4),range(4,8)):
-        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
-    img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+    # Define a color for the cube lines (white in this case)
+    cube_color = (255, 255, 255)  # You can change this to any color you want
+
+    imgpts = np.int32(imgpts).reshape(-1, 2)
+
+    # Draw the base of the cube
+    img = cv2.drawContours(img, [imgpts[:4]], -1, cube_color, 3)
+
+    # Draw the sides of the cube
+    for i, j in zip(range(4), range(4, 8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]), cube_color, 3)
+
+    # Draw the top of the cube
+    img = cv2.drawContours(img, [imgpts[4:8]], -1, cube_color, 3)
+
     return img
 
+
+
 def draw(img, corners, imgpts):
+
     # Ensure the points are in the correct format (integer tuples)
     origin = tuple(imgpts[0].ravel().astype(int))
     x_axis_end = tuple(imgpts[1].ravel().astype(int))
@@ -21,3 +34,13 @@ def draw(img, corners, imgpts):
     img = cv2.line(img, origin, y_axis_end, (0, 255, 0), 5)  # Y-axis in green
     img = cv2.line(img, origin, z_axis_end, (255, 0, 0), 5)  # Z-axis in blue
     return img
+'''
+
+def draw(img, corners, imgpts):
+    corner = tuple(corners[0].ravel())
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+    return img
+
+'''
