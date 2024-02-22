40f9e7e2e67d34993bff3a0f9deb2b7a3e7a3ac6
mamlu007 <76781346+mamlu007@users.noreply.github.com>
2024-02-22 01:00:06 +0100

    task2-partial

--- Assignment 2/auxiliar.py
+++ Assignment 2/auxiliar.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 
 def see_window(window_name, image):
     """
@@ -72,3 +72,82 @@ def preprocess_image(image_aux, optimize_image, kernel_params, canny_thresholds)
     else:
         img = cv2.cvtColor(image_aux, cv2.COLOR_BGR2GRAY)
         return img, image_aux
+
+def averaging_background_model(video_path):
+
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    ret = True
+
+    # Read frames from the video
+    while ret:
+        ret, frame = cap.read()
+        if ret:
+            frames.append(frame)
+
+    # Compute the average of frames if the list is not empty
+    if frames:
+        # Convert the list of frames to a 4D array and compute the average along the first axis
+        avg_frame = np.mean(np.array(frames, dtype=np.float32), axis=0).astype(dtype=np.uint8)
+        # Save the average frame as the background model
+        #cv2.imwrite('background_model.jpg', avg_frame)
+    else:
+        print("No frames to process.")
+
+    # Release the video capture object
+    cap.release()
+    print(f'avg frame: {avg_frame}')
+    return avg_frame
+
+
+def background_model(video_path='background.avi', history=500, varThreshold=25, detectShadows=True):
+    #MOG2 method
+
+    cap = cv2.VideoCapture(video_path)
+    fgbg = cv2.createBackgroundSubtractorMOG2(history=history, varThreshold=varThreshold, detectShadows=detectShadows)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        fgmask = fgbg.apply(frame)
+
+
+    cap.release()
+    return fgmask
+
+
+def subtract_background(frame, background_model):
+    # Convert frame to HSV
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # Convert back
+    hsv_background = cv2.cvtColor(background_model, cv2.COLOR_BGR2HSV)
+    # Calculate the absolute difference
+    diff = cv2.absdiff(hsv_frame, hsv_background)
+
+    print(f'diff: {diff}')
+
+    # Thresholds
+    # These thresholds should be determined based on your method of choice
+   # hue_thresh = 100
+    #sat_thresh = 0
+    #val_thresh = 0
+
+    # Apply thresholding
+  #  _, hue_thresh = cv2.threshold(diff[:, :, 0], hue_thresh, 255, cv2.THRESH_BINARY)
+   # _, sat_thresh = cv2.threshold(diff[:, :, 1], sat_thresh, 255, cv2.THRESH_BINARY)
+    #_, val_thresh = cv2.threshold(diff[:, :, 2], val_thresh, 255, cv2.THRESH_BINARY)
+
+    # Combine the thresholds to determine foreground (use bitwise operations)
+    #combined_mask = cv2.bitwise_and(hue_thresh, cv2.bitwise_and(sat_thresh, val_thresh))
+
+    # Post-processing
+    #kernel = np.ones((5, 5), np.uint8)
+    #erosion = cv2.erode(combined_mask, kernel, iterations=1)
+    #erosion = cv2.erode(diff, kernel, iterations=1)
+    #dilation = cv2.dilate(erosion, kernel, iterations=1)
+
+    return diff
+
+
+
--- Assignment 2/main.py
+++ Assignment 2/main.py
@@ -3,7 +3,7 @@ import numpy as np
 from interpolate import interpolate
 from calibration import calibration, undistort, compute_error
 from detect_corners import find_and_draw_chessboard_corners
-from auxiliar import see_window, extract_frames, preprocess_image
+from auxiliar import see_window, extract_frames, preprocess_image,background_model, subtract_background, averaging_background_model
 import os
 from os import listdir
 
@@ -26,6 +26,25 @@ def calibration():
             if corners2 is None:
                 print("Not corners found for this image.")
             cv2.waitKey(0)
-        
 
-calibration()
+
+def subtraction():
+    camera_folders = ["cam1", "cam2", "cam3", "cam4"]
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    for folder in camera_folders:
+        background_path = os.path.join('data',folder,'background.avi')
+        video_path = os.path.join('data',folder,'video.avi')
+        frames = extract_frames(video_path,interval = 1)
+        for frame in frames:
+            #processed_frame = subtract_background(frame,background_model(background_path))
+            processed_frame = subtract_background(frame, averaging_background_model(video_path))
+            cv2.imshow('Foreground', processed_frame)
+            if cv2.waitKey(0):
+                break
+        cv2.destroyAllWindows()
+
+
+
+subtraction()
+#calibration()
