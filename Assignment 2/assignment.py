98706c97400e5504309446be8fca4160c98a7fbe
amagarzogonzalo <amagarzogonzalo@gmail.com>
2024-02-26 13:12:17 +0100

    .

--- Assignment 2/assignment.py
+++ Assignment 2/assignment.py
@@ -75,11 +75,33 @@ def set_voxel_positions(width, height, depth):
 def get_cam_positions():
     # Generates dummy camera locations at the 4 corners of the room
     # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
+    return [[-64 * block_size, 64 * block_size, 63 * block_size],
+            [63 * block_size, 64 * block_size, 63 * block_size],
+            [63 * block_size, 64 * block_size, -64 * block_size],
+            [-64 * block_size, 64 * block_size, -64 * block_size]], \
+        [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]
+
+
+def get_cam_rotation_matrices():
+    # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
+    # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
+    cam_angles = [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
+    cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
+    for c in range(len(cam_rotations)):
+        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
+        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0])
+        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
+    return cam_rotations
+
+"""
+def get_cam_positions():
+    # Generates dummy camera locations at the 4 corners of the room
+    # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
     camera_folders = ["cam1", "cam2", "cam3", "cam4"]
 
     positions = np.zeros((4, 3))
 
-    for folder in camera_folders():
+    for folder in camera_folders:
         video_path = os.path.join('data', folder, 'video.avi')
         mtx, dist, rvecs, tvecs = get_intrinsics(folder)
 
@@ -100,7 +122,7 @@ def get_cam_rotation_matrices():
     cam_rotations = []
     camera_folders = ["cam1", "cam2", "cam3", "cam4"]
 
-    for folder in camera_folders():
+    for folder in camera_folders:
         video_path = os.path.join('data', folder, 'video.avi')
         mtx, dist, rvecs, tvecs = get_intrinsics(folder)
 
@@ -117,5 +139,7 @@ def get_cam_rotation_matrices():
 
         cam_rotations.append(adjusted_matrix)
 
-    return cam_rotationsate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
+    #return cam_rotationsate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
     return cam_rotations
+
+"""
